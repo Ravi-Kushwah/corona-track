f753ed5440a95743e5ff1b6db8a175a3231844e7
ravi-kushwah <ravikushwah700@gmail.com>
2020-10-07 19:17:27 +0530

    Updates

---
+++ precache-manifest.33c6fefdb4d7286140c80f0e68457a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6c82e8ac4057dd48e151523d91e767",
+    "url": "/corona-track/index.html"
+  },
+  {
+    "revision": "d42aa7932a6549d0ade0",
+    "url": "/corona-track/static/css/main.7b2505d8.chunk.css"
+  },
+  {
+    "revision": "61c3a8ebadabc89743be",
+    "url": "/corona-track/static/js/2.b45ccc08.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-track/static/js/2.b45ccc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42aa7932a6549d0ade0",
+    "url": "/corona-track/static/js/main.fbd8c174.chunk.js"
+  },
+  {
+    "revision": "1059c0f5f4a7bfa16e96",
+    "url": "/corona-track/static/js/runtime-main.4e4ff264.js"
+  }
+]);
